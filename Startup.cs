feda385741e17e3047e0f02da7ac7a5eec99479d
Adrija Basu <Adrija@Adrijas-MacBook-Pro.local>
2020-07-01 17:50:17 +0530

    change1

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace first_app
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Aveek!");
                 });
             });
         }
